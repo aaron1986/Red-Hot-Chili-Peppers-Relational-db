9d406693bdf4ae28377176ef8dd186367bbb95e6
aaron <aaronbrucesmith86@gmail.com>
2026-01-08 20:05:22 +0000

    first commit

--- src/Components/Rhcp.jsx
+++ src/Components/Rhcp.jsx
@@ -3,12 +3,13 @@ import { useEffect, useState } from "react";
 export default function Rhcp() {
   const [music, setMusic] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:9090/api/albums")
-      .then((res) => res.json())
-      .then((data) => setMusic(data.music))
-      .catch((err) => console.error(err));
-  }, []);
+useEffect(() => {
+  fetch(`${import.meta.env.VITE_API_URL}/api/albums`)
+    .then((res) => res.json())
+    .then((data) => setMusic(data.music))
+    .catch((err) => console.error(err));
+}, []);
+
 
   return (
     <div className="albums_page">
